f008c1bbbb0f6c46be982eec58f63946a6315b5e
geovision <zhengxw104@163.com>
2022-12-16 21:31:43 +0800

    Delete local_transforamtion.m

--- local_transforamtion.m
+++
@@ -1,4 +0,0 @@
-function [af,Af,discard,Discard]=local_transforamtion(f1,f2,matches,matches_all)
-    [af,discard] =get_similar(f1(3:4, matches(1,:))', f2(3:4, matches(2,:))');
-    [Af,Discard]=get_similar(f1(3:4, matches_all(1,:))', f2(3:4, matches_all(2,:))');        
-end
